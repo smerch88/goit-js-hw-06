444c8db98899df18baeabdd29bde45b168f0afd1
smerch88 <maxarmyk@gmail.com>
2022-11-01 20:46:55 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,17 +2,12 @@ const validationRef = document.querySelector("#validation-input");
 const lengthRef = validationRef.dataset.length;
 
 validationRef.addEventListener("blur", () => {
-  if (validationRef.value.length >= lengthRef) {
-    if (validationRef.classList.contains("invalid")) {
-      validationRef.classList.replace("invalid", "valid");
-    } else {
-      validationRef.classList.add("valid");
-    }
-  } else {
-    if (validationRef.classList.contains("valid")) {
-      validationRef.classList.replace("valid", "invalid");
-    } else {
-      validationRef.classList.add("invalid");
-    }
-  }
+  if (validationRef.value.length >= lengthRef)
+    return validationRef.classList.contains("invalid")
+      ? validationRef.classList.replace("invalid", "valid")
+      : validationRef.classList.add("valid");
+
+  return validationRef.classList.contains("valid")
+    ? validationRef.classList.replace("valid", "invalid")
+    : validationRef.classList.add("invalid");
 });
