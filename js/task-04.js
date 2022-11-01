61a62d49dbec6a18d5fa17d8d72b99b785df9bae
smerch88 <maxarmyk@gmail.com>
2022-11-01 15:06:51 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,10 +1,12 @@
 const decrementRef = document.querySelector('button[data-action="decrement"]');
 const incrementRef = document.querySelector('button[data-action="increment"]');
 const valueRef = document.querySelector("#value");
+let counter = 0;
 
 decrementRef.addEventListener("click", () => {
-  valueRef.innerHTML -= 1;
+  valueRef.textContent = --counter;
 });
+
 incrementRef.addEventListener("click", () => {
-  valueRef.innerHTML += 1;
+  valueRef.textContent = ++counter;
 });
