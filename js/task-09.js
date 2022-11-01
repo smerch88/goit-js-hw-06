037a56fc60d96274d7ba8f3e46af876bc76e122a
smerch88 <maxarmyk@gmail.com>
2022-11-01 22:14:27 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const buttonRef = document.querySelector(".change-color");
+const spanRef = document.querySelector(".color");
+
+buttonRef.addEventListener(
+  "click",
+  () => (
+    (bodyRef.style.backgroundColor = getRandomHexColor()),
+    (spanRef.textContent = bodyRef.style.backgroundColor)
+  )
+);
