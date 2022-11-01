fcd0836ac86d4fe73f0e0c803bf30157248676f3
smerch88 <maxarmyk@gmail.com>
2022-11-01 21:38:50 +0200

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const fontSizeControlRef = document.querySelector("#font-size-control");
+const textRef = document.querySelector("#text");
+
+fontSizeControlRef.addEventListener(
+  "input",
+  () => (textRef.style.fontSize = `${fontSizeControlRef.value}px`)
+);
