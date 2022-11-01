f38617e6cf3a06738e5ab2fcae9a00e567920b1f
smerch88 <maxarmyk@gmail.com>
2022-11-01 20:36:54 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,18 @@
-document.querySelector();
+const validationRef = document.querySelector("#validation-input");
+const lengthRef = validationRef.dataset.length;
+
+validationRef.addEventListener("blur", () => {
+  if (validationRef.value.length >= lengthRef) {
+    if (validationRef.classList.contains("invalid")) {
+      validationRef.classList.replace("invalid", "valid");
+    } else {
+      validationRef.classList.add("valid");
+    }
+  } else {
+    if (validationRef.classList.contains("valid")) {
+      validationRef.classList.replace("valid", "invalid");
+    } else {
+      validationRef.classList.add("invalid");
+    }
+  }
+});
