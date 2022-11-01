ac2e16348cc7670d18facfa9b81b2152239076c7
smerch88 <maxarmyk@gmail.com>
2022-11-01 22:56:48 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const numberRef = document.querySelector("input");
+const createRef = document.querySelector("[data-create]");
+const destroyRef = document.querySelector("[data-destroy]");
+const boxesRef = document.querySelector("#boxes");
+
+let amount = 0;
+
+numberRef.addEventListener("blur", () => (amount = numberRef.value));
+
+createRef.addEventListener("click", () => {
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.height = `${30 + i * 10}px`;
+    div.style.width = `${30 + i * 10}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxesRef.appendChild(div);
+    console.log(boxesRef.insertAdjacentHTML);
+  }
+});
+
+destroyRef.addEventListener("click", () => {
+  boxesRef.innerHTML = "";
+});
