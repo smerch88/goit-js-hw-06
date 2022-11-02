70d42cc1df9040d9e1d6823da51e7994718cafeb
smerch88 <maxarmyk@gmail.com>
2022-11-02 15:29:14 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,5 +3,8 @@ const outputRef = document.querySelector("#name-output");
 console.log(inputRef);
 
 inputRef.addEventListener("input", () => {
+  if (!inputRef.value) {
+    return (outputRef.textContent = "Anonymous");
+  }
   outputRef.textContent = inputRef.value;
 });
